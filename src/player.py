1efad90f25b5c4be9d72c9d9a442ff6554370b24
HitzeD <d-hitze@comcast.net>
2019-09-04 15:43:55 -0400

    base player

--- src/player.py
+++ src/player.py
@@ -1,2 +1,9 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+
+    def __init__(self, name, currRoom, inv):
+        self.name = name
+        self.currRoom = currRoom
+        self.inv = inv
