e4dc0eba38572fe9d5f95a4a06b26d09bb64e2ed
HitzeD <d-hitze@comcast.net>
2019-09-05 16:04:03 -0400

    gave room directions

--- src/room.py
+++ src/room.py
@@ -6,6 +6,10 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
 
     def __str__(self):
         return f"Room: {self.name}\nDescription: {self.description}"
@@ -13,4 +17,14 @@ class Room:
     def __repr__(self):
         return f'Room: {repr(self.name)}\nDescription: {repr(self.description)}'
 
-    
+    def switch_direction(self, direction):
+        if direction == 'n':
+            return self.n_to
+        elif direction == 's':
+            return self.s_to
+        elif direction == 'e':
+            return self.e_to
+        elif direction == 'w':
+            return self.w_to
+        else:
+            return None
