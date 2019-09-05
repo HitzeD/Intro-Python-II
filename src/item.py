d97167e6fda78af8e73c55e6f4f9c7b520faf6fc
HitzeD <d-hitze@comcast.net>
2019-09-05 19:27:42 -0400

    added __str__ and __repr__

--- src/item.py
+++ src/item.py
@@ -3,3 +3,9 @@ class Item:
     def __init__(self, name, description):
         self.name = name
         self.desciption = description
+
+    def __str__(self):
+        return f" Name: {self.name},\nDescription: {self.desciption}"
+
+    def __repr__(self):
+        return f"Item({repr(self.name)})"
